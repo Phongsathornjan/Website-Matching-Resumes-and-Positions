27725fde3450db242c356e5bc4d21a6a9c36891d
phongsathornjanjamsai@gmail.com <phongsathornjanjamsai@gmail.com>
2024-11-16 23:54:40 +0700

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // ฟังการเชื่อมต่อจากทุก IP
     port: 5173,        // พอร์ตที่ใช้
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4001', // URL ของ backend ที่รันใน EC2 (ให้ใช้ localhost ถ้ารันบนเครื่องเดียวกัน)
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
