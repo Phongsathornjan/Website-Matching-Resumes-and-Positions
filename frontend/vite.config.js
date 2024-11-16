c860c5f3206442fd68990caa18d2f24384e70247
phongsathornjanjamsai@gmail.com <phongsathornjanjamsai@gmail.com>
2024-11-17 00:07:20 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,11 +7,36 @@ export default defineConfig({
     host: '0.0.0.0',  // ฟังการเชื่อมต่อจากทุก IP
     port: 5173,        // พอร์ตที่ใช้
     proxy: {
-      '/': {
-        target: 'http://localhost:4001', // URL ของ backend ที่รันใน EC2 (ให้ใช้ localhost ถ้ารันบนเครื่องเดียวกัน)
-        changeOrigin: true,
-        secure: false,
-      },
+      // Proxy ทุกเส้นทางที่เริ่มต้นด้วย '/api' ไปยัง backend ที่รันใน localhost หรือ EC2
+      '/register': 'http://localhost:4001',
+      '/login': 'http://localhost:4001',
+      '/auth': 'http://localhost:4001',
+      '/tokenDecoder': 'http://localhost:4001',
+      '/resetPassword': 'http://localhost:4001',
+      '/handleRegister': 'http://localhost:4001',
+      '/UpdateJobField': 'http://localhost:4001',
+      '/getInformation': 'http://localhost:4001',
+      '/updateInformation': 'http://localhost:4001',
+      '/uploadPDF': 'http://localhost:4001',
+      '/checkValidResume': 'http://localhost:4001',
+      '/createPost': 'http://localhost:4001',
+      '/deletePost': 'http://localhost:4001',
+      '/getPost': 'http://localhost:4001',
+      '/getPostBySearch': 'http://localhost:4001',
+      '/applyPost': 'http://localhost:4001',
+      '/getPostDetail': 'http://localhost:4001',
+      '/getAppliedJob': 'http://localhost:4001',
+      '/getMostMatchPost': 'http://localhost:4001',
+      '/getMostMatchUser': 'http://localhost:4001',
+      '/getMostMatchFromOutPost': 'http://localhost:4001',
+      '/closePost': 'http://localhost:4001',
+      '/getClosePost': 'http://localhost:4001',
+      '/openPost': 'http://localhost:4001',
+      '/getUserInPostBySearch': 'http://localhost:4001',
+      '/makeAppointment': 'http://localhost:4001',
+      '/CreateInterviewAppointment': 'http://localhost:4001',
+      '/getAppointmentById': 'http://localhost:4001',
+      '/sendOTP': 'http://localhost:4001',
     },
   },
-})
+});
